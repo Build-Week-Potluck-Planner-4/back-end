a01913ec2f85c4b9c20cce8ad3c04485e07acad2
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-09-20 19:16:09 -0400

    added potluck seed

--- api/data/seeds/potlucks.js
+++ api/data/seeds/potlucks.js
@@ -3,9 +3,14 @@ exports.seed = function(knex) {
   return knex('potlucks').del()
     .then(function () {
       return knex('potlucks').insert([
-        {potluck_id: 1, potluck_name: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        { 
+          potluck_id: 1, 
+          potluck_name: 'rowValue1',
+          date: 'date',
+          time: 'time',
+          location: 'location',
+          user_id: 1,
+        },
       ]);
     });
 };
