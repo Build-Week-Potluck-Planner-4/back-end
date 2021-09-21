2ac1e87a5436fa97e95e86671e8e0f180af5d90e
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-09-21 16:49:25 -0400

    added sql comments for later

--- api/potlucks/modal.js
+++ api/potlucks/modal.js
@@ -3,7 +3,13 @@ const db = require("../data/db-config")
 const getAll = () => {
     return db("potlucks as p")
 }
-    
+// for getting foods with potlucks
+// select p.*, f.food_name from potluck_foods as pf
+// left join foods as f
+// on f.food_id = pf.food_id
+// left join potlucks as p
+// on p.potluck_id = pf.potluck_id
+
 const getById = potluck_id => {
     return db("potlucks")
         .where({ potluck_id })
