1a62b3ab5ee43a13cc79be28ac66afb2ad880e1f
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-09-22 13:18:20 -0400

    added some code for getting more potluck details

--- api/potlucks/modal.js
+++ api/potlucks/modal.js
@@ -3,8 +3,37 @@ const db = require("../data/db-config")
 const potluckKeys = ["potluck_name", "date", "time", "location", "potluck_id", "user_id"]
 
 const getAll = () => {
-    return db("potlucks as p")
+    return db("potlucks")
 }
+
+const getPotluck = async potluck_id => {
+    const res = await db("potlucks_foods as pf")
+        .leftJoin("foods as f", "f.food_id", "pf.food_id")
+        .leftJoin("potlucks as p", "p.potluck_id", "pf.potluck_id")
+        .select("p.*, f.food_name")
+        .where("p.potluck_id", potluck_id)
+
+    const guests = await db("guests as g")
+        .leftJoin("users as u", "u.user_id", "g.user_id")
+        .leftJoin("potluck_foods as pf", "pf.guest_id", "g.guest_id")
+        .leftJoin("foods as f", "f.food_id", "pf.food_id")
+        .select("u.username", "f.food_name", "g.accepted")
+        .where("p.potluck_id", potluck_id)
+    
+    return {
+        potluck_id: res[0].potluck_id,
+        potluck_name: res[0].potluck_name,
+        date: res[0].date,
+        time: res[0].time,
+        location: res[0].location,
+        foods: res.map(p => p.food_name),
+        guests: guests.map(g => ({ 
+            ...g, 
+            accepted: g.accepted ? true : false 
+        }))
+    }
+}
+
 // for getting foods with potlucks
 // select p.*, f.food_name from potluck_foods as pf
 // left join foods as f
@@ -35,5 +64,6 @@ module.exports = {
     getById,
     addPotluck,
     getAll,
+    getPotluck,
     remove
 }
