f6c5f31e1befea00b9cdb1ca291e5bf57f75fa6a
Ashley Burns <Ash_burn999@yahoo.com>
2021-09-23 15:00:22 -0400

    had to push to fix heroku you saw changes i made

--- api/guests/modal.js
+++ api/guests/modal.js
@@ -1,32 +1,32 @@
-const db = require("../data/db-config")
+const db = require("../data/db-config");
 
 const addGuest = (potluck_id, username) => {
     return db("users")
         .where({ username }, ["user_id"])
         .first()
-        .then(({ user_id }) => 
-            db("guests").insert({ 
-                potluck_id, 
-                user_id, 
+        .then(({ user_id }) =>
+            db("guests").insert({
+                potluck_id,
+                user_id,
             })
-        )
-}
+        );
+};
 
 
 const getById = guest_id => {
     return db("guests")
         .where({ guest_id })
-        .first()
-}
+        .first();
+};
 
-const guestGoing = (guest_id, going) => {
+const guestGoing = (guest_id, guest) => {
     return db("guests")
-        .update({ accepted: going })
-        .where({ guest_id })
-}
+        .update(guest)
+        .where({ guest_id });
+};
 
 module.exports = {
     addGuest,
     guestGoing,
     getById
-}
+};
